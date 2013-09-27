33089d199f375fa2e5ad9c4ef1e9f81ef70d714f
Henry AT <yiperu@gmail.com>
2013-09-26 20:41:33 -0500

    Se configuraron en el Delegate para que se pueda acceder al view , a esto llamariamos instalnciar el controller en el delagate

--- ViewControllerProgramatically/ViewControllerProgramatically/AppDelegate.m
+++ ViewControllerProgramatically/ViewControllerProgramatically/AppDelegate.m
@@ -9,12 +9,17 @@
 #import "AppDelegate.h"
 
 @implementation AppDelegate
+@synthesize rootViewController;
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     // Override point for customization after application launch.
-    self.window.backgroundColor = [UIColor whiteColor];
+    
+    self.rootViewController = [[UIViewController alloc] initWithNibName:@"Vista" bundle:nil];
+    [self.window addSubview:self.rootViewController.view];
+    
+    self.window.backgroundColor = [UIColor whiteColor];    
     [self.window makeKeyAndVisible];
     return YES;
 }
