92fcf733cd299faf05e563e5c59036eb7d61eb8d
Henry AT <yiperu@gmail.com>
2013-09-26 19:45:27 -0500

    Agregamos el ViewController sin vista a la aplicacion

---
+++ ViewControllerProgramatically/ViewControllerProgramatically/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ViewControllerProgramatically
+//
+//  Created by Henry AT on 9/26/13.
+//  Copyright (c) 2013 Henry AT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ ViewControllerProgramatically/ViewControllerProgramatically/ViewController.m
@@ -0,0 +1,38 @@
+//
+//  ViewController.m
+//  ViewControllerProgramatically
+//
+//  Created by Henry AT on 9/26/13.
+//  Copyright (c) 2013 Henry AT. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
